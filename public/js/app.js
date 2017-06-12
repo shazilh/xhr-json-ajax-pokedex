9bcb9a335ba3f171b6b7d0357b970bf9e82e7839
Shazil Tovar <shazil@gmail.com>
2017-06-12 16:47:02 -0500

    primera version terminada

--- public/js/app.js
+++ public/js/app.js
@@ -1,108 +1,86 @@
-var cargarPagina = function(){
-  cargarPokemons();
-  $(document).on("click", ".pokemon", mostrarDetallePokemon);
-    //cuando los elementos son creados dinamicamente utilizar el evento on click
-    $('.modal').modal();
-}
-
-var cargarPokemons = function(){
-  var url ="http://pokeapi.co/api/v2/pokemon/";
-  $.getJSON(url,function(response){
-    var pokemones = response.results;
-    var total = response.count;
-    mostrarTotalPokemones(total);
-    mostrarPokemones(pokemones);
-  });
-};
-var mostrarTotalPokemones = function(total){
-  $("#total").text(total)
-}
-
-var mostrarPokemones = function(pokemones){
-  var $ul= $("#pokemones");
-    //console.log(personajes);
-  // crear un li que se muestre en el html
-  pokemones.forEach(function(pokemon){//por defecto nos va a dar una unidad, obtiene el valor de cada objeto
-      console.log(pokemon);
-    var $li = $("<li/>");
-   // $li.text(personaje.name);
-    $li.addClass("pokemon");
-    $li.attr("data-url",pokemon.ability);//le da el atributo de data-url a cada personaje
-    $li.text(pokemon.name + " - " + pokemon.weight + "kg");   //crea un elemento li para cada nombre de personaje
-    $ul.append($li);       //agrega cada personaje a la lista
-      //console.log(personaje.name);  //obtenemos solo el nombre de los personajes.
-  });
+var cargarPagina = function () {
+	$.getJSON("https://pokeapi.co/api/v2/pokemon/",
+		function (response) {
+		var pokemons = response.results;
+		crearPokemons(pokemons);
+        //console.log(pokemons);
+		$(".modal").modal();
+		$('.contenedorModal').click(masInformacion);
+        
+	});
     
-}
-var plantillaPokemon =  '<h2>Pokemon:</h2>' +
-   '<p><strong>Nombre:</strong>__nombre__</p>' +
-   '<p><strong>Habilidad:</strong>__habilidad__</p>';
+var plantillaPokemones='<div class=" col s2 contenedorModal"  data-url="__url__" data-img="__imgPokemon__" >'+
+		'<a href="#modalPokemon">'+
+				'	<img src="__urlImagenPokemon__" alt="pokemon" class="responsive-img col s12 borderojo">'+
+				'<span class="col s12">__nombreDePokemon__</span>'+
+			'</a>'+
+		'</div>';
+var imagenesPokemones=['./img/bulbasaur.png',
+		'./img/Ivysaur.png','./img/venusaur.png','./img/Charmander.png','./img/charmeleon.jpeg',
+		'./img/charizard.png','./img/Squirtle.png','./img/Wartortle.png','./img/blastoise.png',
+		'./img/Caterpie.png','./img/Metapod.png','./img/250px-012Butterfree.png','./img/weedle.png',
+		'./img/Kakuna.png','./img/Beedrill.png','./img/Pidgey.png','./img/Pidgeotto.png',
+		'./img/Pidgeot.png','./img/Rattata.png','./img/Raticate.png'];
 
-var mostrarDetallePokemon=function(){
-    var url=$(this).data("url");
-    console.log(url);
-    var $pokemonContenedor = $("#pokemones");
-    $.getJSON(url,function(response){
-     $pokemonContenedor.html(
-     plantillaPokemon.replace('__nombre__',response.name).replace('__habilidad__',response.ability)
-        );
-    });
+var plantillaModal = '<div class="modal-content ">'+
+		'<div class="col s12 m7 color">'+
+			 '<h2 class="">__nombreDePokemon__</h2>'+
+			 ' <div class="card horizontal row">'+
+				 '<div class="card-image col s6 m6 ">'+
+					'<img src="__imagenPokemon__" class="imagenModal responsive-img circle">'+
+				 '</div>'+
+				 '<div class="container">'+
+					 	'<div class="row">'+
+						 '<p class="col s12 m6">color:__color__</p>'+
+						 '<p class="col s12 m6">habitat:__habitat__</p>'+
+						 '<p class="col s12 m6">shape:__shape__</p>'+
+						 '<p class="col s12 m6">generacion:__generacion__</p>'
+					 '</div>'+
+				 '</div>'+
+			 '</div>'+
+		 '</div>'+
+	'</div>';
+	
+
+var obtenerDatos = function(pokemon,imagen){
+	 var nombre = pokemon.names[0].name;
+	 var color = pokemon.color.name;
+	 var habitat = pokemon.habitat.name;
+	 var forma = pokemon.shape.name;
+	 var generacion = pokemon.generation.name;
+	 var plantillaFinal ="";
+	 plantillaFinal+=plantillaModal.replace('__nombreDePokemon__',nombre)
+		 .replace('__color__',color)
+		 .replace('__habitat__',habitat)
+		 .replace('__shape__',forma)
+		 .replace('__generacion__', generacion)
+		 .replace('__imagenPokemon__',imagen);
+		 $("#modalPokemon").html(plantillaFinal);
+ }
+
+var masInformacion = function(){
+      var urlPokemonEspecie=$(this).data('url').replace('pokemon','pokemon-species').replace("http://",'https://');
+			 console.log(urlPokemonEspecie);
+			 console.log($(this).data("img"));
+      var imagen = $(this).data("img");
+			 $("#modalPokemon").html();
+			 $.getJSON(urlPokemonEspecie,function(response){
+				 obtenerDatos(response,imagen);
+			 });
 };
-$(document).ready(cargarPagina);  
 
+var crearPokemons=function(pokemons) {
+    
+	plantillaPokemonFinal = "";
+	pokemons.forEach(function (pokemon,i) {
+    plantillaPokemonFinal+=plantillaPokemones.replace("__nombreDePokemon__",pokemon.name)
+		.replace("__urlImagenPokemon__",imagenesPokemones[i])
+		.replace("__url__",pokemon.url)
+		.replace("__imgPokemon__",imagenesPokemones[i]);
+	});
 
+	$('#pokes').html(plantillaPokemonFinal);
+}
+}
 
-
-
-
-
-
-
-
-
-
-//$.getJSON("http://pokeapi.co/api/v2/pokemon/", 
-	//function (response) {
-	//var pokemons = response.results;
-	//crearPokemons(pokemons);
-//});
-
-// var xhr = new XMLHttpRequest();
-
-// xhr.onreadystatechange = function (e) {
-// 	if (this.readyState === 4) {
-// 		if (this.status === 200) {
-			// var response = JSON.parse(this.response);
-			// var pokemons = response.results;
-			// var squads = JSON.parse(this.response);	
-			// crearPokemons(pokemons);
-// 		}
-		
-// 	}
-// };
-
-// xhr.open("GET", "http://pokeapi.co/api/v2/pokemon/");
-
-// xhr.send();
-
-//function crearPokemons(pokemons) {
-//	var ul = document.getElementById("pokemons");
-
-//	pokemons.forEach(function (pokemon) {
-//		var li = document.createElement("li");
-//		li.textContent = pokemon.name;
-
-//		ul.appendChild(li);
-//	});
-//}
-//
-
-
-
-
-
-
-
-
-
-
+cargarPagina();
